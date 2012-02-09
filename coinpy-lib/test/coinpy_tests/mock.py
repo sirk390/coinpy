c181fdf5e3084ea628b46a70be8166040128c945
Christian Bodt <sirk390@gmail.com>
2012-02-09 13:09:50 +0100

    -Add accept_all_methods and accept_all_args parameters to mock

--- coinpy-lib/test/coinpy_tests/mock.py
+++ coinpy-lib/test/coinpy_tests/mock.py
@@ -10,7 +10,8 @@ class Mock():
     __init__
         specs :   {name: {args: return_value, ...}, {name: return_value, ...}, ...}
     """
-    def __init__(self, specs={}):
+    def __init__(self, specs={}, accept_all_methods=False):
+        self.accept_all_methods = accept_all_methods
         self._methods = {}
         for name, spec in specs.iteritems():
             if type(spec) is not dict:
@@ -20,8 +21,11 @@ class Mock():
     def __getattr__(self, name):
         if name in self._methods:
             return self._methods[name]
-        raise AttributeError("Mock method undefined: %s" % (name))
-        
+        if not self.accept_all_methods:
+            raise AttributeError("Mock method undefined: %s" % (name))
+        self._methods[name] = MockMethod(accept_all_args=True)
+        return self._methods[name]
+    
     def __str__(self):
         return "{Mock}"
     
@@ -30,9 +34,13 @@ class MockMethod():
     __init__
         spec: {args: return_value, ...}
                args can be a tuple, list or single item
+        accept_all:
+               if False, raises an exception if arguments are not defined in 
+               spec, if True, returns a Mock() object.
     """
-    def __init__(self, spec):
+    def __init__(self, spec={}, accept_all_args=False):
         self.call_count = 0
+        self.accept_all_args = accept_all_args
         self._spec = {}
         for args, return_value in spec.iteritems():
             if (type(args) is not tuple) and (type(args) is not list):
@@ -43,14 +51,25 @@ class MockMethod():
         self.call_count += 1
         if args in self._spec:
             return self._spec[args]
-        raise Exception("Mock result undefined")
+        if not self.accept_all_args:
+            raise Exception("Mock result_value undefined for argument")
+        return Mock()
     
     called = property(lambda self: self.call_count != 0)
     
 if __name__ == '__main__':
+    m1 = MockMethod(spec={"a": 6, "b": 1})  
+    print m1("b")
+    m1 = MockMethod(accept_all_args=True)  
+    print m1("c")
+    
     a1 = Mock(specs={"xx": 6})              #simple return value
     a2 = Mock(specs={"xx": {"a":1, "b":2}}) #return value depends on parameter
     a3 = Mock(specs={"xx": {("a", 6) :1}})  #return value depends on multiple parameters
+    a4 = Mock(accept_all_methods=True)      #accept any method
+    
+    a4.test("a")
+    assert a4.test.called
     
     b = Mock(specs={"xx": 6, "yy":{"a": 1,("b",2) : 2}})
     print b.xx()
