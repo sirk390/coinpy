ae75ebfab72eb449e5031e403cc875bbbdfaa59b
Christian Bodt <sirk390@gmail.com>
2012-02-13 23:22:23 +0100

    -Add wallet database

---
+++ coinpy-lib/src/coinpy/lib/database/bsddb_env.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 11 Feb 2012
+
+@author: kris
+"""
+import bsddb
+from bsddb.db import DB_CREATE, DB_INIT_LOCK, DB_INIT_LOG, DB_INIT_MPOOL, \
+    DB_INIT_TXN, DB_THREAD, DB_RECOVER
+
+class BsdDbEnv(object):
+    def __init__(self, directory):
+        self.directory = directory
+        self.dbenv = bsddb.db.DBEnv()
+        
+        self.dbenv.set_lg_max(10000000)
+        self.dbenv.set_lk_max_locks(10000)
+        self.dbenv.set_lk_max_objects(10000)
+        self.dbenv.open(directory,
+                          DB_CREATE|DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_MPOOL|
+                           DB_INIT_TXN|DB_THREAD|DB_RECOVER)
