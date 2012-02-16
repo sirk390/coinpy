f0946f3c1fe0b6a212caf4688e287861bcd4e6cb
Christian Bodt <sirk390@gmail.com>
2012-02-16 20:46:55 +0100

    -fix bugs due to rename of reactor file

--- coinpy-client/src/coinpy_client/gui/bitcoin_client.py
+++ coinpy-client/src/coinpy_client/gui/bitcoin_client.py
@@ -4,13 +4,12 @@ Created on 13 Feb 2012
 
 @author: kris
 """
-from coinpy.node.network.reactor import Reactor
+from coinpy.tools.reactor.reactor import Reactor
 from coinpy.lib.database.bsddb_env import BSDDBEnv
 from coinpy.model.genesis import GENESIS
 from coinpy.lib.database.blockchain.db_blockchain import BSDDbBlockChainDatabase
 from coinpy.lib.bitcoin.blockchain.blockchain import Blockchain
 from coinpy.lib.bitcoin.blockchain_with_pools import BlockchainWithPools
-from coinpy.lib.database.wallet.wallet_database import WalletDatabase
 from coinpy.lib.bitcoin.wallet.wallet import Wallet
 from coinpy.lib.bootstrap.bootstrapper import Bootstrapper
 from coinpy.node.bitcoinnode import BitcoinNode
@@ -19,6 +18,7 @@ import os
 from coinpy.tools.observer import Observable
 from coinpy.node.network.sockaddr import SockAddr
 from coinpy.node.network.bitcoin_port import BITCOIN_PORT
+from coinpy.lib.database.wallet.bsddb_wallet_database import BSDDBWalletDatabase
 
 class BitcoinClient(Observable):
     EVT_WALLET_OPENED = Observable.createevent()
@@ -32,7 +32,7 @@ class BitcoinClient(Observable):
         self.dbenv_handles = {}     # directory => DBEnv
         
         self.nodeparams = nodeparams
-        self.reactor = Reactor(self.log)
+        self.reactor = Reactor()
         #blockchain database
         self.database = BSDDbBlockChainDatabase(self.log, self.get_dbenv_handle(data_directory), self.nodeparams.runmode, data_directory)
         self.database.open_or_create(GENESIS[self.nodeparams.runmode])
@@ -54,8 +54,8 @@ class BitcoinClient(Observable):
     
     def open_wallet(self, filename):
         directory, basename = os.path.split(filename)
-        wallet_db = WalletDatabase(self.get_dbenv_handle(directory), filename)
-        wallet = Wallet(wallet_db, self.blockchain)
+        wallet_db = BSDDBWalletDatabase(self.get_dbenv_handle(directory), filename)
+        wallet = Wallet(wallet_db)
         self.wallets.append(wallet)
         self.wallet_filenames[wallet] = filename
         self.fire(self.EVT_WALLET_OPENED, wallet=wallet, filename=filename)
