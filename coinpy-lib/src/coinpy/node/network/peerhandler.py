53443fc676bb1499898d7b78646331781381554a
Christian Bodt <sirk390@gmail.com>
2012-04-25 12:15:13 +0200

    -Fix issue with "[Errno 9] Bad file descriptor" on immediately disconnecting peers

--- coinpy-lib/src/coinpy/node/network/peerhandler.py
+++ coinpy-lib/src/coinpy/node/network/peerhandler.py
@@ -3,15 +3,21 @@ import socket
 import traceback
 from coinpy.tools.observer import Observable
 
-class PeerHandler(asyncore.dispatcher_with_send, Observable):
+"""
+Note: We are not using dispatcher_with_send as it doesn't take into account 
+writability before sending data (bug of calling initiate_send() in dispatcher_with_send.send() ?)
+This very small feature is reimplemented here.
+"""
+class PeerHandler(asyncore.dispatcher, Observable):
     EVT_CONNECT = Observable.createevent()
     EVT_DISCONNECT = Observable.createevent()
    
     def __init__(self, sockaddr, sock=None):
         Observable.__init__(self)
-        asyncore.dispatcher_with_send.__init__(self, sock=sock)
+        asyncore.dispatcher.__init__(self, sock=sock)
         self.sockaddr = sockaddr
         self.isoutbound = (sock == None)
+        self.out_buffer = ""
         if not sock:
             self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
             self.connect((sockaddr.ip, sockaddr.port))
@@ -25,6 +31,16 @@ class PeerHandler(asyncore.dispatcher_with_send, Observable):
     def handle_close(self):
         self.fire(self.EVT_DISCONNECT)
         self.close()
-        
+
+    def writable(self):
+        return (not self.connected) or len(self.out_buffer)
+
+    def handle_write(self):
+        num_sent = asyncore.dispatcher.send(self, self.out_buffer[:512])
+        self.out_buffer = self.out_buffer[num_sent:]
+               
+    def send(self, data):
+        self.out_buffer = self.out_buffer + data
+
     def __str__(self):
         return str(self.sockaddr)
